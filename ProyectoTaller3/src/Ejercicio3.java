f231fd9e8e659f8b0bd35d92ff95b2c1620d6df5
Pabl794 <pavelez11@utpl.edu.ec>
2024-10-24 20:52:16 -0500

    Ejercicio3P

--- ProyectoTaller3/src/Ejercicio3.java
+++ ProyectoTaller3/src/Ejercicio3.java
@@ -1,12 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author UTPL
- */
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+      int v1, v2, v3, v4, v5, v6;
+      double respuesta;
+      v1 = 10;
+      v2= 2;
+      v3= 16; 
+      v4 = 20;
+      v5 = 1;
+      v6 = 11;
+      respuesta = (Math.pow(v1, v2)+Math.sqrt(16)+ 20/1+11);
+      System.out.println("respuesta = " + respuesta);
+      
+              
+              }
 }
