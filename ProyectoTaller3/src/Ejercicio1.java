5f0b8df49dc6e6cc183797ccdbaedd8aace69835
Pabl794 <pavelez11@utpl.edu.ec>
2024-10-24 20:05:14 -0500

    Taller03_ejercicios vacios

---
+++ ProyectoTaller3/src/Ejercicio1.java
@@ -0,0 +1,15 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        int v1, v2, v3, v4, v5, v6;
+        double respuest;
+        v1= 1;
+        v2= 2;
+        v3 = 4;
+        v4 = 3;
+        v5 = 5;
+        v6 = 81;
+        respuest =((v1+v2+v3+v4)/v5) + Math.sqrt(v6);
+        System.out.println("respuest = " + respuest);
+                
+    }
+}
