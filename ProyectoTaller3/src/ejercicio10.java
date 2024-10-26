b9c1e8dc8abffca43931f8a2798d5d3b94e182c8
Elizabeth2007-create <chunchograndavaleriaelizabeth@gmail.com>
2024-10-25 21:30:06 -0500

    commit3

--- ProyectoTaller3/src/ejercicio10.java
+++ ProyectoTaller3/src/ejercicio10.java
@@ -1,4 +1,21 @@
 
 public class ejercicio10 {
+    public static void main(String[] args) {
+    double v1, v2, v3, v6, v7, v8;
+    boolean v4, v5, respuesta;
+    v1 = 25;
+    v2 = 10;
+    v3 = 100;
+    v4 = true;
+    v5 = false;
+    v6 = 10;
+    v7 = 5;
+    v8 = 2;
+    respuesta= ((Math.sqrt(v1)*v2 >= v3) && (v4) || (v5) || (v6 / v7 >= v8));
+        System.out.println("respuesta = " + respuesta);
+            
+            
     
+    
+    }
 }
